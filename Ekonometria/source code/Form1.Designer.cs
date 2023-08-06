8bbb8b67053ba2c28e6e1e984ff471671194db9a
AgnieszkaM2 <72810860+AgnieszkaM2@users.noreply.github.com>
2023-08-06 14:46:48 +0200

    Initial commit

---
+++ Ekonometria/source code/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace Ekonometria
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelZmienne = new System.Windows.Forms.Label();
+            this.labelOdchylenie = new System.Windows.Forms.Label();
+            this.labelD = new System.Windows.Forms.Label();
+            this.labelRownanie = new System.Windows.Forms.Label();
+            this.labelDet = new System.Windows.Forms.Label();
+            this.labelLos = new System.Windows.Forms.Label();
+            this.btFile = new System.Windows.Forms.Button();
+            this.btResult = new System.Windows.Forms.Button();
+            this.textBoxDane = new System.Windows.Forms.TextBox();
+            this.groupBoxR0 = new System.Windows.Forms.GroupBox();
+            this.labelR0 = new System.Windows.Forms.Label();
+            this.panel = new System.Windows.Forms.Panel();
+            this.labelEDa = new System.Windows.Forms.Label();
+            this.groupBoxR = new System.Windows.Forms.GroupBox();
+            this.labelR = new System.Windows.Forms.Label();
+            this.label_dane_check = new System.Windows.Forms.Label();
+            this.labelVal = new System.Windows.Forms.Label();
+            this.groupBoxR0.SuspendLayout();
+            this.panel.SuspendLayout();
+            this.groupBoxR.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dane:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(645, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Wyniki:";
+            // 
+            // labelZmienne
+            // 
+            this.labelZmienne.AutoSize = true;
+            this.labelZmienne.Location = new System.Drawing.Point(14, 171);
+            this.labelZmienne.Name = "labelZmienne";
+            this.labelZmienne.Size = new System.Drawing.Size(70, 20);
+            this.labelZmienne.TabIndex = 2;
+            this.labelZmienne.Text = "Zmienne:";
+            // 
+            // labelOdchylenie
+            // 
+            this.labelOdchylenie.AutoSize = true;
+            this.labelOdchylenie.Location = new System.Drawing.Point(14, 205);
+            this.labelOdchylenie.Name = "labelOdchylenie";
+            this.labelOdchylenie.Size = new System.Drawing.Size(342, 20);
+            this.labelOdchylenie.TabIndex = 3;
+            this.labelOdchylenie.Text = "Odchylenie standardowe składnika resztowego Su:";
+            // 
+            // labelD
+            // 
+            this.labelD.AutoSize = true;
+            this.labelD.Location = new System.Drawing.Point(14, 239);
+            this.labelD.Name = "labelD";
+            this.labelD.Size = new System.Drawing.Size(41, 20);
+            this.labelD.TabIndex = 4;
+            this.labelD.Text = "D(a):";
+            // 
+            // labelRownanie
+            // 
+            this.labelRownanie.AutoSize = true;
+            this.labelRownanie.Location = new System.Drawing.Point(14, 323);
+            this.labelRownanie.Name = "labelRownanie";
+            this.labelRownanie.Size = new System.Drawing.Size(162, 20);
+            this.labelRownanie.TabIndex = 5;
+            this.labelRownanie.Text = "Równanie modelu:  Y= ";
+            // 
+            // labelDet
+            // 
+            this.labelDet.AutoSize = true;
+            this.labelDet.Location = new System.Drawing.Point(14, 385);
+            this.labelDet.Name = "labelDet";
+            this.labelDet.Size = new System.Drawing.Size(189, 20);
+            this.labelDet.TabIndex = 6;
+            this.labelDet.Text = "Współczynnik determinacji:";
+            // 
+            // labelLos
+            // 
+            this.labelLos.AutoSize = true;
+            this.labelLos.Location = new System.Drawing.Point(14, 418);
+            this.labelLos.Name = "labelLos";
+            this.labelLos.Size = new System.Drawing.Size(246, 20);
+            this.labelLos.TabIndex = 7;
+            this.labelLos.Text = "Współczynnik zmienności losowej v:";
+            // 
+            // btFile
+            // 
+            this.btFile.Location = new System.Drawing.Point(83, 22);
+            this.btFile.Name = "btFile";
+            this.btFile.Size = new System.Drawing.Size(158, 60);
+            this.btFile.TabIndex = 8;
+            this.btFile.Text = "Otwórz plik";
+            this.btFile.UseVisualStyleBackColor = true;
+            this.btFile.Click += new System.EventHandler(this.btFile_Click);
+            // 
+            // btResult
+            // 
+            this.btResult.Location = new System.Drawing.Point(589, 22);
+            this.btResult.Name = "btResult";
+            this.btResult.Size = new System.Drawing.Size(163, 60);
+            this.btResult.TabIndex = 9;
+            this.btResult.Text = "Oblicz";
+            this.btResult.UseVisualStyleBackColor = true;
+            this.btResult.Click += new System.EventHandler(this.btResult_Click);
+            // 
+            // textBoxDane
+            // 
+            this.textBoxDane.Location = new System.Drawing.Point(2, 153);
+            this.textBoxDane.Multiline = true;
+            this.textBoxDane.Name = "textBoxDane";
+            this.textBoxDane.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxDane.Size = new System.Drawing.Size(376, 300);
+            this.textBoxDane.TabIndex = 10;
+            this.textBoxDane.TextChanged += new System.EventHandler(this.textBoxDane_TextChanged);
+            // 
+            // groupBoxR0
+            // 
+            this.groupBoxR0.Controls.Add(this.labelR0);
+            this.groupBoxR0.Location = new System.Drawing.Point(43, 13);
+            this.groupBoxR0.Name = "groupBoxR0";
+            this.groupBoxR0.Size = new System.Drawing.Size(160, 145);
+            this.groupBoxR0.TabIndex = 11;
+            this.groupBoxR0.TabStop = false;
+            this.groupBoxR0.Text = "R0";
+            // 
+            // labelR0
+            // 
+            this.labelR0.AutoSize = true;
+            this.labelR0.Location = new System.Drawing.Point(6, 23);
+            this.labelR0.Name = "labelR0";
+            this.labelR0.Size = new System.Drawing.Size(0, 20);
+            this.labelR0.TabIndex = 16;
+            // 
+            // panel
+            // 
+            this.panel.Controls.Add(this.labelEDa);
+            this.panel.Controls.Add(this.groupBoxR0);
+            this.panel.Controls.Add(this.groupBoxR);
+            this.panel.Controls.Add(this.labelLos);
+            this.panel.Controls.Add(this.labelDet);
+            this.panel.Controls.Add(this.labelZmienne);
+            this.panel.Controls.Add(this.labelOdchylenie);
+            this.panel.Controls.Add(this.labelD);
+            this.panel.Controls.Add(this.labelRownanie);
+            this.panel.Location = new System.Drawing.Point(384, 153);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(657, 455);
+            this.panel.TabIndex = 13;
+            // 
+            // labelEDa
+            // 
+            this.labelEDa.AutoSize = true;
+            this.labelEDa.Location = new System.Drawing.Point(14, 355);
+            this.labelEDa.Name = "labelEDa";
+            this.labelEDa.Size = new System.Drawing.Size(136, 20);
+            this.labelEDa.TabIndex = 16;
+            this.labelEDa.Text = "Oszacowane błędy:";
+            // 
+            // groupBoxR
+            // 
+            this.groupBoxR.Controls.Add(this.labelR);
+            this.groupBoxR.Location = new System.Drawing.Point(221, 13);
+            this.groupBoxR.Name = "groupBoxR";
+            this.groupBoxR.Size = new System.Drawing.Size(420, 145);
+            this.groupBoxR.TabIndex = 12;
+            this.groupBoxR.TabStop = false;
+            this.groupBoxR.Text = "R";
+            // 
+            // labelR
+            // 
+            this.labelR.AutoSize = true;
+            this.labelR.Location = new System.Drawing.Point(20, 23);
+            this.labelR.Name = "labelR";
+            this.labelR.Size = new System.Drawing.Size(0, 20);
+            this.labelR.TabIndex = 17;
+            // 
+            // label_dane_check
+            // 
+            this.label_dane_check.AutoSize = true;
+            this.label_dane_check.Location = new System.Drawing.Point(12, 476);
+            this.label_dane_check.Name = "label_dane_check";
+            this.label_dane_check.Size = new System.Drawing.Size(193, 20);
+            this.label_dane_check.TabIndex = 14;
+            this.label_dane_check.Text = "Poprawność danych w pliku:";
+            // 
+            // labelVal
+            // 
+            this.labelVal.AutoSize = true;
+            this.labelVal.Location = new System.Drawing.Point(48, 508);
+            this.labelVal.Name = "labelVal";
+            this.labelVal.Size = new System.Drawing.Size(0, 20);
+            this.labelVal.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 638);
+            this.Controls.Add(this.labelVal);
+            this.Controls.Add(this.btFile);
+            this.Controls.Add(this.label_dane_check);
+            this.Controls.Add(this.panel);
+            this.Controls.Add(this.textBoxDane);
+            this.Controls.Add(this.btResult);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Program_ekonometria";
+            this.groupBoxR0.ResumeLayout(false);
+            this.groupBoxR0.PerformLayout();
+            this.panel.ResumeLayout(false);
+            this.panel.PerformLayout();
+            this.groupBoxR.ResumeLayout(false);
+            this.groupBoxR.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelZmienne;
+        private System.Windows.Forms.Label labelOdchylenie;
+        private System.Windows.Forms.Label labelD;
+        private System.Windows.Forms.Label labelRownanie;
+        private System.Windows.Forms.Label labelDet;
+        private System.Windows.Forms.Label labelLos;
+        private System.Windows.Forms.Button btFile;
+        private System.Windows.Forms.Button btResult;
+        private System.Windows.Forms.TextBox textBoxDane;
+        private System.Windows.Forms.GroupBox groupBoxR0;
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.GroupBox groupBoxR;
+        private System.Windows.Forms.Label label_dane_check;
+        private System.Windows.Forms.Label labelR0;
+        private System.Windows.Forms.Label labelR;
+        private System.Windows.Forms.Label labelEDa;
+        private System.Windows.Forms.Label labelVal;
+    }
+}
+
---
+++ Ekonometria/source code/Form1.cs
@@ -0,0 +1,333 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using MathNet.Numerics.LinearAlgebra;
+using System.IO;
+
+namespace Ekonometria
+{
+    public partial class Form1 : Form
+    {
+
+        private int iloscZmiennych;
+
+        private int iloscWierszy;
+
+        private double[] Y;
+
+        private double[] Ywyliczane;
+
+        private double[][] X_i;
+
+        private double[] R0;
+
+        private double[][] R;
+
+        private double[][] X;
+
+        public class Macierz
+        {
+            public List<int> Items { get; set; }
+
+            public double H { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            btResult.Enabled = false;
+        }
+
+		public Matrix<double> matrix_X(double[,] tablica)
+		{
+			return Matrix<double>.Build.DenseOfArray(tablica);
+		}
+
+		public void get_H(Macierz macierz)
+		{
+			for (int i = 0; i < macierz.Items.Count; i++)
+			{
+				double num = 0.0;
+				for (int j = 0; j < macierz.Items.Count; j++)
+				{
+					if (i != j)
+					{
+						num += Math.Abs(R[macierz.Items[i] - 1][macierz.Items[j] - 1]);
+					}
+				}
+				macierz.H += R0[macierz.Items[i] - 1] * R0[macierz.Items[i] - 1] / (1.0 + num);
+			}
+		}
+
+		public IEnumerable<IEnumerable<T>> combinationsCount<T>(IEnumerable<T> items, int ilosc)
+		{
+			int i = 0;
+			foreach (T item in items)
+			{
+				if (ilosc == 1)
+				{
+					yield return new T[1] { item };
+				}
+				else
+				{
+					using IEnumerator<IEnumerable<T>> elem1 = combinationsCount(items.Skip(i + 1), ilosc - 1).GetEnumerator();
+					while (elem1.MoveNext())
+					{
+						yield return Enumerable.Concat(second: elem1.Current, first: new T[1] { item });
+					}
+				}
+				i++;
+			}
+		}
+
+		private double Correlations(double[] wektor, double[] wektor2)
+		{
+			double n1 = 0.0;
+			double n2 = 0.0;
+			double n3 = 0.0;
+			for (int i = 0; i < wektor.Count(); i++)
+			{
+				double n4 = wektor[i] - wektor.Average();
+				double n5 = wektor2[i] - wektor2.Average();
+				n1 += n4 * n5;
+				n2 += n4 * n4;
+				n3 += n5 * n5;
+			}
+			return n1 / Math.Sqrt(n2 * n3);
+		}
+
+		private void btFile_Click(object sender, EventArgs e)
+		{
+			Stream stream = null;
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Filter = "Txt files (*.txt)|*.txt|All files (*.*)|*.*";
+			openFileDialog.RestoreDirectory = true;
+			if (openFileDialog.ShowDialog() != DialogResult.OK)
+			{
+				return;
+			}
+			if ((stream = openFileDialog.OpenFile()) != null)
+			{
+				using (StreamReader streamReader = new StreamReader(stream))
+				{
+					textBoxDane.Text = streamReader.ReadToEnd();
+					return;
+				}
+			}
+
+		}
+
+		private void textBoxDane_TextChanged(object sender, EventArgs e)
+        {
+			bool isValid = true;
+			string[] wiersze = textBoxDane.Lines;
+			iloscWierszy = wiersze.Where((string x) => x.Length > 0).Count();
+			List<List<string>> tekst = (from x in wiersze where x.Length > 0 select x.Split(new char[1] { ' ' }, StringSplitOptions.RemoveEmptyEntries).ToList()).ToList();
+			if (tekst.Any())
+			{
+				iloscZmiennych = tekst.FirstOrDefault().Count - 1;
+			}
+			if (tekst.Any((List<string> x) => x.Count() != tekst.FirstOrDefault().Count()))
+			{
+				isValid = false;
+			}
+			if (iloscZmiennych < 1)
+			{
+				isValid = false;
+			}
+			if (tekst.Count() == 0)
+			{
+				isValid = false;
+			}
+			if (isValid)
+			{
+				Y = new double[iloscWierszy];
+				X_i = new double[iloscZmiennych][];
+				List<int> list = new List<int>();
+				for (int i = 0; i < X_i.Length; i++)
+				{
+					X_i[i] = new double[iloscWierszy];
+				}
+				for (int j = 0; j < tekst.Count(); j++)
+				{
+					for (int k = 0; k < tekst[j].Count; k++)
+					{
+						double result = 0.0;
+						if (double.TryParse(tekst[j][k].Replace('.', ','), out result))
+						{
+							if (k == 0)
+							{
+								Y[j] = result;
+							}
+							else
+							{
+								X_i[k - 1][j] = result;
+							}
+						}
+						else
+						{
+							isValid = false;
+						}
+					}
+				}
+			}
+			if (isValid)
+			{
+				labelVal.Text = "Dane poprawne";
+				btResult.Enabled = true;
+			}
+			else
+			{
+				labelVal.Text = "Zły format danych";
+				btResult.Enabled = false;
+			}
+		}
+
+
+        private void btResult_Click(object sender, EventArgs e)
+        {
+			// Wektor i macierz korelacji
+			R0 = new double[iloscZmiennych];
+			R = new double[iloscZmiennych][];
+			for (int i = 0; i < iloscZmiennych; i++)
+			{
+				R0[i] = Correlations(Y, X_i[i]);
+			}
+			for (int j = 0; j < iloscZmiennych; j++)
+			{
+				R[j] = new double[iloscZmiennych];
+				for (int k = 0; k < iloscZmiennych; k++)
+				{
+					R[j][k] = Correlations(X_i[j], X_i[k]);
+				}
+			}
+			labelR0.Text = "";
+			for (int i1 = 0; i1 < iloscZmiennych; i1++)
+			{
+				Label label2 = labelR0;
+				label2.Text = label2.Text + $"{R0[i1]:0.0000}" + "\n";
+			}
+			labelR.Text = "";
+			for (int i2 = 0; i2 < iloscZmiennych; i2++)
+			{
+				for (int j2 = 0; j2 < iloscZmiennych; j2++)
+				{
+					if (R[i2][j2] >= 0.0)
+					{
+						labelR.Text += " ";
+					}
+					labelR.Text += $"{R[i2][j2]:0.0000}";
+					labelR.Text += "  ";
+				}
+				labelR.Text += "\n";
+			}
+			//ilość kombinacji dla zmiennych, H
+			Macierz[] array = new Macierz[(int)Math.Pow(2.0, iloscZmiennych) - 1];
+			List<int> list = new List<int>();
+			for (int l = 0; l < iloscZmiennych; l++)
+			{
+				list.Add(l + 1);
+			}
+			int n1 = 0;
+			for (int m = 0; m < iloscZmiennych; m++)
+			{
+				IEnumerable<IEnumerable<int>> enumerable = combinationsCount(list, m + 1);
+				foreach (IEnumerable<int> item in enumerable)
+				{
+					array[n1] = new Macierz();
+					array[n1].Items = item.ToList();
+					get_H(array[n1]);
+					n1++;
+				}
+			}
+			Macierz macierz = array.OrderByDescending((Macierz x) => x.H).FirstOrDefault();
+			X = new double[macierz.Items.Count + 1][];
+			for (int n = 0; n < macierz.Items.Count; n++)
+			{
+				X[n] = new double[iloscZmiennych];
+				X[n] = X_i[macierz.Items[n] - 1];
+			}
+			X[macierz.Items.Count] = new double[iloscWierszy];
+			for (int n2 = 0; n2 < iloscWierszy; n2++)
+			{
+				X[macierz.Items.Count][n2] = 1.0;
+			}
+			labelZmienne.Text = "Zmienne:  ";
+			foreach (int item in macierz.Items)
+			{
+				Label label = labelZmienne;
+				label.Text = label.Text + "x" + item + ", ";
+			}
+			//szacowanie D(a)
+			Matrix<double> matrix = Matrix<double>.Build.DenseOfColumnArrays(X);
+			Vector<double> one = Vector<double>.Build.DenseOfArray(Y);
+			Matrix<double> matrix2 = Matrix<double>.Build.DenseOfColumnArrays(Y);
+			int count = 0;
+			foreach (int item in macierz.Items)
+			{
+				count++;
+			}
+			Vector<double> vector = matrix.Transpose().Multiply(matrix).Inverse().Multiply(matrix.Transpose().Multiply(one));
+			Matrix<double> other = Matrix<double>.Build.DenseOfColumnVectors(vector);
+			Matrix<double> matrix3 = 1.0 / (double)(iloscWierszy - (count + 1)) * (matrix2.Transpose().Multiply(matrix2) - matrix2.Transpose().Multiply(matrix).Multiply(other));
+			Matrix<double> matrix4 = matrix3[0, 0] * (matrix.Transpose() * matrix).Inverse();
+			double[] array2 = new double[matrix4.RowCount];
+			for (int n3 = 0; n3 < matrix4.RowCount; n3++)
+			{
+				array2[n3] = Math.Sqrt(matrix4[n3, n3]);
+			}
+			for (int n4 = 0; n4 < array2.Length; n4++)
+			{
+				Label label = labelD;
+				label.Text = label.Text + "D(a" + (n4 + 1) + ") = " + $"{array2[n4]:0.0000}" + "\n";
+			}
+			labelEDa.Text = "Oszacowane błędy:     ";
+			for (int n8 = 0; n8 < array2.Length; n8++)
+			{
+				labelEDa.Text += "(";
+				labelEDa.Text += $"{array2[n8]:0.0000}";
+				labelEDa.Text += ")    ";
+			}
+			//odchylenie, los
+			double odch = Math.Sqrt(matrix3[0, 0]);
+			double los = odch / Y.Average();
+			labelLos.Text = "Współczynnik zmienności losowej:  " + $"{los:0.0000}";
+			Ywyliczane = new double[iloscWierszy];
+			for (int n5 = 0; n5 < iloscWierszy; n5++)
+			{
+				for (int n6 = 0; n6 < macierz.Items.Count; n6++)
+				{
+					Ywyliczane[n5] += vector[n6] * X_i[macierz.Items[n6] - 1][n5];
+				}
+				Ywyliczane[n5] += vector.Last();
+			}
+			double first = 0.0;
+			double second = 0.0;
+			for (int n7 = 0; n7 < iloscWierszy; n7++)
+			{
+				first += (Ywyliczane[n7] - Y.Average()) * (Ywyliczane[n7] - Y.Average());
+				second += (Y[n7] - Y.Average()) * (Y[n7] - Y.Average());
+			}
+			//rownanie
+			labelRownanie.Text = "Równanie modelu: Y = ";
+			for (int r = 0; r < vector.Count; r++)
+			{
+				Label label = labelRownanie;
+				label.Text = label.Text + $"{vector[r]:0.0000}" + "x" + macierz.Items[r] + " + ";
+				if (r == vector.Count - 2)
+				{
+					r++;
+					labelRownanie.Text += $"{vector[r]:0.0000}";
+				}
+			}
+			///labels
+
+			labelDet.Text = "Współczynnik determinacji:  " + $"{(first / second):0.0000}";
+			labelOdchylenie.Text = "Odchylenie standardowe składnika resztowego:  " + $"{odch:0.0000}";
+
+		}
+
+    }
+}
